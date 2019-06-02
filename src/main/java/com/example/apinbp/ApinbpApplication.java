1c21a1461e85b920065ad8bd42d44fcdd5582ec9
stefan <sawicadam@gmail.com>
2019-06-02 12:02:45 +0200

    first commit

---
+++ src/main/java/com/example/apinbp/ApinbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.apinbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApinbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApinbpApplication.class, args);
+    }
+
+}
